0df5e133ec3149c1801cf3c9e9fdab5b71d108d9
Avik Jain <avikjain12345@gmail.com>
2017-10-03 17:27:05 -0700

    trying to move to openshift 3

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongodbUrl: (process.env.OPENSHIFT_MONGODB_DB_URL || 'localhost/')+(process.env.OPENSHIFT_APP_NAME || 'mvjsa')
+  mongodbUrl: (process.env.OPENSHIFT_MONGODB_DB_URL || 'localhost/')+(process.env.MONGODB_DATABASE || 'mvjsa')
 }
